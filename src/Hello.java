429604ff9ed85a5496d97bb1de35748151027830
Marcin <marcin96semkowski@gmail.com>
2019-03-12 21:31:21 +0100

    Section: 3 - Variables,Data types and Operations

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,17 @@ public class Hello {
 
     public static void main(String[] args){
 
-       System.out.println("Hello world !");
+      int myFirstNumber = (10 + 5) +  (10 * 2);
+      int mySecondNumber = 12;
+      int myThirdNumber = myFirstNumber * 2;
+
+      int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+      int myLastOne = 1000 - myTotal;
+
+      System.out.println(myTotal);
+
+
 
     }
 
