6ccff63af9f1a9c787c5dd879321c16d64bf3851
Marcin <marcin96semkowski@gmail.com>
2019-06-04 19:25:56 +0200

    Section 14: #3

--- src/Basic_Input_Output_including_java/util/L_2/Locations.java
+++ src/Basic_Input_Output_including_java/util/L_2/Locations.java
@@ -3,18 +3,51 @@ package Basic_Input_Output_including_java.util.L_2;
 import Basic_Input_Output_including_java.util.L_4.IndexRecord;
 
 import java.io.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 /**
  * Created by timbuchalka on 2/04/2016.
  */
 public class Locations implements Map<Integer, Location> {
-    private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
+    private static Map<Integer, Location> locations = new LinkedHashMap<>();
     private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
     private static RandomAccessFile ra;
 
     public static void main(String[] args) throws IOException {
 
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))) {
+            for(Location location : locations.values()) {
+                locFile.writeObject(location);
+            }
+        }
+
+
+
+        /*
+        Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
+        Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
+        try(BufferedWriter locFile = Files.newBufferedWriter(dirPath);
+          BufferedWriter dirFile = Files.newBufferedWriter(dirPath)){
+         for(Location location : locations.values()){
+             locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+           for(String direction : location.getExits().keySet()){
+               if(!direction.equalsIgnoreCase("Q")){
+                   dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n" );
+
+               }
+           }
+         }
+        }catch(IOException e){
+            System.out.println("IOException: " + e.getMessage());
+        }
+
+         */
+        /*
         try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
             rao.writeInt(locations.size());
             int indexSize = locations.size() * 3 * Integer.BYTES;
@@ -55,16 +88,72 @@ public class Locations implements Map<Integer, Location> {
 
         }
 
-    }
+         */
 
-    // 1. This first four bytes will contain the number of locations (bytes 0-3)
-    // 2. The next four bytes will contain the start offset of the locations section (bytes 4-7)
-    // 3. The next section of the file will contain the index (the index is 1692 bytes long.  It will start at byte 8 and end at byte 1699
-    // 4. The final section of the file will contain the location records (the data). It will start at byte 1700
+    }
 
 
     static {
-        try {
+
+
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(locPath)))) {
+            boolean eof = false;
+            while(!eof) {
+                try {
+                    Location location = (Location) locFile.readObject();
+                    locations.put(location.getLocationID(), location);
+                } catch(EOFException e) {
+                    eof = true;
+                }
+            }
+        } catch(InvalidClassException e) {
+            System.out.println("InvalidClassException " + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("IOException " + e.getMessage());
+        } catch(ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException " + e.getMessage());
+        }
+
+
+        /*
+        Path locPath  = FileSystems.getDefault().getPath("locations_big.txt");
+        Path dirPath  = FileSystems.getDefault().getPath("directions_big.txt");
+
+        try(Scanner scanner  = new Scanner(Files.newBufferedReader(locPath))) {
+          scanner.useDelimiter(",");
+         while(scanner.hasNextLine()){
+             int loc  = scanner.nextInt();
+             scanner.skip(scanner.delimiter());
+             String descrption  = scanner.nextLine();
+             System.out.println("Imported loc: " + loc + ": " + descrption);
+             locations.put(loc,new Location(loc,descrption,null));
+
+
+         }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try(BufferedReader dirFile = Files.newBufferedReader(dirPath)) {
+            String input ;
+            while((input = dirFile.readLine()) != null){
+                String[] data = input.split(",");
+                int loc  = Integer.parseInt(data[0]);
+                String direction =data[1];
+                int destination  = Integer.parseInt(data[2]);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction,destination);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+
+        /* try {
             ra = new RandomAccessFile("locations_rand.dat", "rwd");
             int numLocations = ra.readInt();
             long locationStartPoint = ra.readInt();
@@ -81,6 +170,7 @@ public class Locations implements Map<Integer, Location> {
         } catch(IOException e) {
             System.out.println("IOException in static initializer: " + e.getMessage());
         }
+      */
     }
 
     public Location getLocation(int locationId) throws IOException {
