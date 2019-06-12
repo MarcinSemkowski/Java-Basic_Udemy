0407f24572dca43aece66dd59e15a7953d2fdd81
Marcin <marcin96semkowski@gmail.com>
2019-06-12 19:11:16 +0200

    Section 19: #1

---
+++ src/Databases/L_2/Mail.java
@@ -0,0 +1,29 @@
+package Databases.L_2;
+
+import Databases.L_2.model.Artist;
+import Databases.L_2.model.Datasource;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if(artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for(Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        datasource.close();
+    }
+}
